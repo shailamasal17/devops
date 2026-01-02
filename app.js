635b934e1fef621bcdf10a11213ef6bdbd8323ea
Sanket <sanketyele100@gmail.com>
2026-01-02 16:32:38 +0530

    final text change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Jenkins  =================================== Auto Deploy Working Successfully");
+  res.send("Jenkins  Successfully");
 });
 
 app.listen(3000, () => {

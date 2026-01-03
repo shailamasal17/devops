d24cec62538c83a642b154ca6adbbd829f59b800
sanket yele <sanketyele100@gmail.com>
2026-01-03 06:47:31 +0530

    Update response message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Jenkins  Successfully");
+  res.send("Jenkinnnnnnnnnnnnnnnnns  Successfully");
 });
 
 app.listen(3000, () => {
